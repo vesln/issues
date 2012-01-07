8b0da13e3c1ae2814e879b2b271b9f8aeac9b188
Veselin Todorov <veselin@veselin.bg>
2012-01-07 11:10:54 +0200

    Adding github tests

---
+++ test/github.test.js
@@ -0,0 +1,35 @@
+/*!
+ * GitHub Issues from the CLI.
+ * 
+ * Veselin Todorov <hi@vesln.com>
+ * MIT License.
+ */
+ 
+/**
+ * Dependencies.
+ */
+var github = require('../lib/github');
+
+describe('github', function() {
+  describe('repos', function() {
+    it('should return all repos for user', function(done) {
+      github.repos('vesln', function(err, repos) {
+        repos.should.be.ok;
+        Array.isArray(repos).should.be.ok;
+        repos.indexOf('logme').should.be.ok;
+        (repos.indexOf('test-foo-bar') < 0).should.be.ok;
+        done();
+      });
+    });
+  });
+  
+  describe('issues', function() {
+    it('should return issues for repo', function(done) {
+      github.issues('vesln', ['logme'], function(err, issues) {
+        issues.logme.should.be.ok
+        Array.isArray(issues.logme).should.be.ok;
+        done();
+      });
+    });
+  });
+});
